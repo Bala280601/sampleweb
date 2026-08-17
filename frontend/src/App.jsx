66d8adb76cdcfb59ca698e5253a080268c62aa36
Ubuntu <balasubramaniyaraja280601@gmail.com>
2026-08-17 14:46:26 +0000

    DevOps project files addedssssss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { AlertCircle, CheckCircle2 } from 'lucide-react';
 
 const BACKEND_URL =
   import.meta.env.VITE_BACKEND_URL ||
-  'http://13.201.36.45:30080';
+  'http://13.233.111.250:30080';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home');
